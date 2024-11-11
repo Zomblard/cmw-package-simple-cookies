b5e83c70d171f123671279982356ec5af42292f5
Teyir <thomas.txr435@gmail.com>
2024-11-11 19:04:27 +0100

    [IMPROVES] Entities extend AbstractEntity + clean code

--- Entities/SimpleCookiesSettingsEntity.php
+++ Entities/SimpleCookiesSettingsEntity.php
@@ -2,13 +2,9 @@
 
 namespace CMW\Entity\SimpleCookies;
 
-use JsonException;
-use RuntimeException;
-use function json_decode;
-use function json_encode;
-use const JSON_THROW_ON_ERROR;
+use CMW\Manager\Package\AbstractEntity;
 
-class SimpleCookiesSettingsEntity
+class SimpleCookiesSettingsEntity extends AbstractEntity
 {
     private string $bannerTitle;
     private string $bannerText;
@@ -48,55 +44,5 @@ class SimpleCookiesSettingsEntity
     public function getPageContent(): string
     {
         return $this->pageContent;
-    }
-
-    /**
-     * @return string
-     */
-    public function toJson(): string
-    {
-        $data = [
-            'banner_title' => $this->bannerTitle,
-            'banner_text' => $this->bannerText,
-            'page_content' => $this->pageContent,
-        ];
-
-        try {
-            return json_encode($data, JSON_THROW_ON_ERROR);
-        } catch (JsonException $e) {
-            throw new \RuntimeException("Can't convert data to json. " . $e->getMessage());
-        }
-    }
-
-    /**
-     * @param string $data
-     * @return self
-     */
-    public static function fromJson(string $data): self
-    {
-        try {
-            $json = json_decode($data, true, 512, JSON_THROW_ON_ERROR);
-        } catch (JsonException $e) {
-            throw new RuntimeException("Can't convert json to data. " . $e->getMessage());
-        }
-
-        return new self(
-            $json['banner_title'],
-            $json['banner_text'],
-            $json['page_content'],
-        );
-    }
-
-    /**
-     * @param array $data
-     * @return self
-     */
-    public static function toEntity(array $data): self
-    {
-        return new self(
-            $data['banner_title'],
-            $data['banner_text'],
-            $data['page_content'],
-        );
     }
 }
